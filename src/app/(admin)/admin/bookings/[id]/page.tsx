42bfcb0e2cb15b6c6e77c8ec55f5909123298192
nablus11 <mireranablus3@gmail.com>
2026-03-20 15:28:45 +0300

    fix: remove empty booking detail page

--- src/app/(admin)/admin/bookings/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function BookingDetailPage({ params }: { params: { id: string } }) {
-  return (
-    <div>
-      <h1>Booking Detail</h1>
-      <p>Booking ID: {params.id}</p>
-    </div>
-  );
-}
